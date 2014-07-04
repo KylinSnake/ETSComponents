6bdac91e52720bccddcccf69604875c9d347f005
Roy Li <ruyunli@gmail.com>
2014-07-04 09:18:10 +0800

    Add future, impl, service

--- net/net-core/src/main/java/org/ruyunli/components/net/core/future/ReadFutureInterface.java
+++
@@ -1,8 +0,0 @@
-package org.ruyunli.components.net.core.future;
-
-/**
- * Created by Roy on 2014/6/25.
- */
-public interface ReadFutureInterface
-{
-}
--- net/net-core/src/main/java/org/ruyunli/components/net/core/session/SessionManagerImplInterface.java
+++
@@ -1,8 +0,0 @@
-package org.ruyunli.components.net.core.session;
-
-/**
- * Created by Roy on 2014/6/25.
- */
-interface SessionManagerImplInterface
-{
-}
